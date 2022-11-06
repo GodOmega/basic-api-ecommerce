ee858178272ef32a0ba0c8637da9c522cece73c0
= <=>
2022-11-06 18:24:23 -0400

    adding modules to api route

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 // Modules Routes
 const productsRouter= require('../../modules/products/router')
 const categoriesRouter= require('../../modules/categories/router')
+const cartsRouter= require('../../modules/carts/router')
 
 router.use('/products', productsRouter);
 router.use('/categories', categoriesRouter);
+router.use('/carts', cartsRouter);
 
 
 module.exports = router;
