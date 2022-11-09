c968cdfb26973423a9e96983b8cf18756c719238
= <=>
2022-11-09 19:18:16 -0400

    Adding orders routes

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -5,10 +5,12 @@ const router = express.Router();
 const productsRouter= require('../../modules/products/router')
 const categoriesRouter= require('../../modules/categories/router')
 const cartsRouter= require('../../modules/carts/router')
+const ordersRouter= require('../../modules/orders/router')
 
 router.use('/products', productsRouter);
 router.use('/categories', categoriesRouter);
 router.use('/carts', cartsRouter);
+router.use('/orders', ordersRouter);
 
 
 module.exports = router;
