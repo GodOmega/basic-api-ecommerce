b7ff2fc932da98a18a3e45490b353762153c474d
= <=>
2022-11-13 17:28:35 -0400

    Adding error handling to cart module

--- src/modules/carts/router.js
+++ src/modules/carts/router.js
@@ -10,89 +10,75 @@ router.delete("/:id", deleteCart);
 router.post("/:id/add-item", addOrUpdateCartItem);
 router.post("/:id/delete-item", deleteCartItem);
 
-async function getAll(req, res) {
+async function getAll(req, res, next) {
   try {
     const response = await service.getAll();
     res.json(response);
   } catch (error) {
-    res.json({
-      message: error,
-    });
+    next(error);
   }
 }
 
-async function getCart(req, res) {
+async function getCart(req, res, next) {
   try {
     const { id } = req.params;
     const response = await service.getOne(id);
     res.json(response);
   } catch (error) {
-    res.json({
-      message: error,
-    });
+    next(error);
   }
 }
 
-async function createCart(req, res) {
+async function createCart(req, res, next) {
   try {
     const { body } = req;
     const newCart = await service.create(body);
     res.status(201).json(newCart);
   } catch (error) {
-    res.json({
-      message: error,
-    });
+    next(error);
   }
 }
 
-async function updateCart(req, res) {
+async function updateCart(req, res, next) {
   try {
     const { id } = req.params;
     const { body } = req;
     const response = await service.update(id, body);
     res.json(response);
   } catch (error) {
-    res.json({
-      message: error,
-    });
+    next(error);
   }
 }
 
-async function deleteCart(req, res) {
+async function deleteCart(req, res, next) {
   try {
     const { id } = req.params;
     const response = await service.delete(id);
     res.json(response);
   } catch (error) {
-    res.json({
-      message: error,
-    });
+    next(error);
   }
 }
 
-async function addOrUpdateCartItem(req, res) {
+async function addOrUpdateCartItem(req, res, next) {
   try {
     const { id } = req.params;
     const { body } = req;
     const response = await service.addOrUpdateItem(id, body);
     res.json(response);
   } catch (error) {
-    res.json({
-      message: error,
-    });
+    next(error);
   }
 }
 
-async function deleteCartItem(req, res) {
+async function deleteCartItem(req, res, next) {
   try {
     const { id } = req.params;
     const { productId } = req.body;
     const response = await service.deleteItem(id, productId);
     res.json(response);
   } catch (error) {
-    res.json({
-      message: error,
-    });
+    next(error);
   }
 }
 
