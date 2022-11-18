0c26e42540d451d32763da21c2412d23653bf3c6
= <=>
2022-11-18 10:07:26 -0400

    fixing pagination functions

--- src/utils/getPaginatedStructure.js
+++ src/utils/getPaginatedStructure.js
@@ -1,7 +1,7 @@
 module.exports = (query, page, limit) => {
   const { count: totalItems, rows: data } = query;
 
-  const currentPage = page ? parseInt(page) : 0;
+  const currentPage = page ? parseInt(page) : 1;
   const totalPages = Math.ceil(totalItems / limit);
 
   return { totalItems, data, totalPages, currentPage };
--- src/utils/getPaginationValues.js
+++ src/utils/getPaginationValues.js
@@ -1,6 +1,6 @@
 module.exports = (page, size) => {
   const limit = size ? parseInt(size) : 2;
-  const offset = page ? parseInt(page * limit) : 0;
+  const offset = page ? parseInt((page - 1) * limit) : 0;
 
   return { limit, offset };
 };
