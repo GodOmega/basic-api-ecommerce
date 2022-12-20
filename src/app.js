1f1845976106bff4261fc82a30a912a48242c452
= <=>
2022-12-19 21:37:18 -0400

    [ADD] swagger initial setup

--- src/app.js
+++ src/app.js
@@ -1,22 +1,27 @@
-const express = require('express')
+const express = require("express");
 const helmet = require("helmet");
-const app = express()
+const swaggerUi = require("swagger-ui-express");
+const app = express();
 
-
-const {errorHandler, boomErrorHandler} = require('./middlewares/errors.handler')
-const setRoutes = require('./routes')
+const swaggerSetup = require("./swagger");
+const {
+  errorHandler,
+  boomErrorHandler,
+} = require("./middlewares/errors.handler");
+const setRoutes = require("./routes");
 
 // CONFIG
 app.use(helmet());
 app.use(express.json());
 
 // ROUTES
-setRoutes(app)
+setRoutes(app);
+
+// Setup SWAGGER DOC
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSetup));
 
 // Handling Erros
-
-app.use(boomErrorHandler)
-app.use(errorHandler)
-
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 module.exports = app;
---
+++ src/swagger/index.js
@@ -0,0 +1,71 @@
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerDefinition = {
+  openapi: "3.0.0",
+  info: {
+    title: "Ecommerce API",
+    version: "1.0.0",
+  },
+  servers: [
+    {
+      url: "http://localhost:3000",
+    },
+  ],
+  components: {
+    schemas: {
+      product: {
+        type: "object",
+        required: [
+          "title",
+          "description",
+          "slug",
+          "sku",
+          "categoryId",
+          "quantity",
+          "price",
+        ],
+        properties: {
+          title: {
+            type: "string",
+          },
+          description: {
+            type: "string",
+          },
+          slug: {
+            type: "string",
+          },
+          sku: {
+            type: "string",
+          },
+          categoryId: {
+            type: "number",
+          },
+          quantity: {
+            type: "number",
+          },
+          quantity: {
+            type: "number",
+          },
+        },
+      },
+      category: {
+        type: "object",
+        required: ["name", "description"],
+        properties: {
+            name: {
+            type: "string",
+          },
+          description: {
+            type: "string",
+          },
+        },
+      },
+    },
+  },
+};
+
+const swaggerOptions = {
+  swaggerDefinition,
+  apis: ["./src/modules/**/router.js"],
+};
+
+module.exports = swaggerJSDoc(swaggerOptions);
