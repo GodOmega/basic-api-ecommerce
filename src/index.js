bee45c47628ec4fdddbb0c22c8cc165787157f98
= <=>
2022-12-21 17:24:04 -0400

    [ADD] swagger init log

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ const {port} = require('./config')
 
 app.listen(port, () => {
     console.log(`APP RUNNING IN PORT: ${port}`)
+    console.log(`API docs available in: http://localhost:${port}/docs`)
 })
--- src/swagger/index.js
+++ src/swagger/index.js
@@ -42,7 +42,7 @@ const swaggerDefinition = {
           quantity: {
             type: "number",
           },
-          quantity: {
+          price: {
             type: "number",
           },
         },
